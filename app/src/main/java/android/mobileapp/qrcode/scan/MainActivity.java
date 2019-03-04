8984bc589d492870e087c9271768cb123930c08b
lorence <lorence@enclave.vn>
2019-03-04 11:04:19 +0700

    Custom QR full screen

--- app/src/main/java/android/mobileapp/qrcode/scan/MainActivity.java
+++ app/src/main/java/android/mobileapp/qrcode/scan/MainActivity.java
@@ -5,10 +5,11 @@ import android.app.Activity;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.graphics.Bitmap;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.annotation.RequiresApi;
 import android.support.v4.app.ActivityCompat;
@@ -16,14 +17,9 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.os.Handler;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.zxing.Result;
@@ -33,24 +29,22 @@ import me.dm7.barcodescanner.zxing.ZXingScannerView;
 import static android.Manifest.permission.CAMERA;
 
 public class MainActivity extends AppCompatActivity implements ZXingScannerView.ResultHandler {
-    private static final int UI_ANIMATION_DELAY = 300;
+    private static final int UI_ANIMATION_DELAY = 0;
     private final Handler mHideHandler = new Handler();
-    private View mContentView;
 
     private static final int REQUEST_CAMERA = 1;
     private ZXingScannerView scannerView;
-
-    private TextView tvHi, tvDescription;
-    private ImageView container;
-    private LinearLayout layout;
-
-    private RenderScriptGaussianBlur blur;
 
     private final Runnable mHidePart2Runnable = new Runnable() {
         @SuppressLint("InlinedApi")
         @Override
         public void run() {
-            mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE | View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+            scannerView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
+                    | View.SYSTEM_UI_FLAG_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
         }
     };
     private final Runnable mShowPart2Runnable = new Runnable() {
@@ -62,7 +56,6 @@ public class MainActivity extends AppCompatActivity implements ZXingScannerView.
             }
         }
     };
-    private boolean mVisible;
     private final Runnable mHideRunnable = new Runnable() {
         @Override
         public void run() {
@@ -74,48 +67,27 @@ public class MainActivity extends AppCompatActivity implements ZXingScannerView.
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        setContentView(R.layout.activity_main);
-
-        mVisible = true;
-        mContentView = findViewById(R.id.fullscreen_content);
-
-        mContentView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                toggle();
-            }
-        });
-
-        tvHi = findViewById(R.id.tvHi);
-        tvDescription = findViewById(R.id.tvDescription);
-
-        layout = findViewById(R.id.layout);
-        container = findViewById(R.id.container);
-
-        container.setVisibility(View.GONE);
-
-        blur = new RenderScriptGaussianBlur(MainActivity.this);
-
-        initComponents();
+        scannerView = findViewById(R.id.zXingScannerView);
+        setContentView(R.layout.activity_camera);
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             setStatusBarGradient(this);
         }
 
-//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-//            if (checkPermission()) {
-//                launchApplication();
-//            } else {
-//                requestPermissions();
-//            }
-//        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkPermission()) {
+                launchApplication();
+            } else {
+                requestPermissions();
+            }
+        }
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-//        launchApplication();
+        delayedHide(0);
+        launchApplication();
     }
 
     private void launchApplication() {
@@ -145,7 +117,6 @@ public class MainActivity extends AppCompatActivity implements ZXingScannerView.
             Window window = activity.getWindow();
             Drawable background = activity.getResources().getDrawable(R.drawable.custom_bg);
             window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-            window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
                 window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR);
             }
@@ -154,37 +125,9 @@ public class MainActivity extends AppCompatActivity implements ZXingScannerView.
     }
 
     @Override
-    public void onWindowFocusChanged(boolean hasFocus) {
-        super.onWindowFocusChanged(hasFocus);
-        container.setVisibility(View.VISIBLE);
-
-        layout.setDrawingCacheEnabled(true);
-        layout.setDrawingCacheQuality(View.DRAWING_CACHE_QUALITY_LOW);
-
-        Bitmap bitmap = layout.getDrawingCache();
-
-        container.setImageBitmap(blur.gaussianBlur(25, bitmap));
-
-        layout.setVisibility(View.VISIBLE);
-    }
-
-    private void initComponents() {
-        tvHi.setTypeface(OverrideFonts.getTypeFace(getApplicationContext(), OverrideFonts.TYPE_FONT_NAME.HELVETICANEUE, OverrideFonts.TYPE_STYLE.BOLD));
-        tvDescription.setTypeface(OverrideFonts.getTypeFace(getApplicationContext(), OverrideFonts.TYPE_FONT_NAME.HELVETICANEUE, OverrideFonts.TYPE_STYLE.LIGHT));
-    }
-
-    @Override
     protected void onPostCreate(Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
-        delayedHide(100);
-    }
-
-    private void toggle() {
-        if (mVisible) {
-            hide();
-        } else {
-            show();
-        }
+        delayedHide(0);
     }
 
     private void hide() {
@@ -192,19 +135,9 @@ public class MainActivity extends AppCompatActivity implements ZXingScannerView.
         if (actionBar != null) {
             actionBar.hide();
         }
-        mVisible = false;
 
         mHideHandler.removeCallbacks(mShowPart2Runnable);
         mHideHandler.postDelayed(mHidePart2Runnable, UI_ANIMATION_DELAY);
-    }
-
-    @SuppressLint("InlinedApi")
-    private void show() {
-        mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION);
-        mVisible = true;
-
-        mHideHandler.removeCallbacks(mHidePart2Runnable);
-        mHideHandler.postDelayed(mShowPart2Runnable, UI_ANIMATION_DELAY);
     }
 
     private void delayedHide(int delayMillis) {
